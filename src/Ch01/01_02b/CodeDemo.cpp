12ab0d00985decd7dc98c3d8f8304c72f74e6b82
Anna McNamara <137454511+AMCreates@users.noreply.github.com>
2025-05-27 20:39:42 +0000

    Section One Changes

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,21 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+// iostream is a part of the C++ standard library
+// it contains objects and functions to print text to the screen and recieve text from the keyboard
+// Pre-processor directives do not end with a semicolon
+#include <iostream>
+
+// "main" is the name of the function; it is case sensitive
+// "()" represents an empty parameter list; The main function may recieve nothing or a set of strings that come from the command line
+int main(){
+    // "::" = Scope Resolution Operator
+    // "cout" = C Out is short for Character Output; Represents the output window
+    // "<<" = Left-Bound Insertion Operator; Enter whatever message you want to send to the object at the left
+    // "endl" = Short for End Line; 
+    std::cout << "Hi There" << std::endl; 
+    
+    std::cout << std::endl << std::endl; // Prints out two End Line Characters to seperate my output from the debugger's exit message; Makes it easier to read
+    return(0);  // main is a function that returns an integer, so we will return 0 since it is traditionally used to mean that no errors came up in the program
+}
--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -3,13 +3,14 @@
 // Console Interaction, by Eduardo Corpeño 
 
 #include <iostream>
-#include <string>
+#include <string> // We want to get a string from the user, so we need to call this library
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::string str; // str is a string variable we will use to hold the user's input
+    std::cout << "Please type out your name. Press the enter key when finished. " << std::flush; // "flush" makes sure that the output is completely sent to the terminal before waiting for the user input
+    std::cin >> str; // This line puts the user input in str; cin only works for single words
+    std::cout << "Nice to meet you, " << str << "!" << std::endl;  // This line prints str
 
-    std::cout << std::endl << std::endl;
+    std::cout << std::endl << std::endl; // Adds an empty line between my text and the end of message text
     return (0);
 }
